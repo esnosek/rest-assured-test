43435a26d8c05e026c75cd994829b42170bb4343
esnosek <esnosek@gmail.com>
2017-11-17 15:53:32 +0100

    clean test

--- src/main/java/app/config/MongoConfig.java
+++ src/main/java/app/config/MongoConfig.java
@@ -13,7 +13,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 
 @Configuration
 @EnableMongoRepositories(basePackages = "app.repository")
-public class MongoConfig extends AbstractMongoConfiguration implements InitializingBean{
+public class MongoConfig extends AbstractMongoConfiguration {
 
     @Override
     public String getDatabaseName() {
@@ -29,21 +29,5 @@ public class MongoConfig extends AbstractMongoConfiguration implements Initializ
     @Bean
     public MongoTemplate mongoTemplate() throws Exception {
         return new MongoTemplate(mongo(), "library");
-    }
-
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-//        mongoTemplate().remove(new Query(), "books");
-//        Book book1 = new Book("1001", "Ogniem i Mieczem", "Sienkiewicz", 1880);
-//        Book book2 = new Book("1002","W Pustyni i w puszczy", "Sienkiewicz", 1870);
-//        Book book3 = new Book("1003","Zbrodnia i Kara", "Dostojewski", 1888);
-//        Book book4 = new Book("1004","Bracia Karamazow", "Dostojewski", 1875);
-//        Book book5 = new Book("1005","Wprowadzenie do algorytmów", "Cormen", 1980);
-//        mongoTemplate().save(book1);
-//        mongoTemplate().save(book2);
-//        mongoTemplate().save(book3);
-//        mongoTemplate().save(book4);
-//        mongoTemplate().save(book5);
     }
 }
